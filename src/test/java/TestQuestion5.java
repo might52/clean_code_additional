0f7e5adacf3f0c7c1bcbe58171512f6fdd0e11eb
Andrei F._ <answers_com@hotmail.com>
2024-09-25 11:08:29 +0300

    fix generation of the stream for tests.

--- src/test/java/TestQuestion5.java
+++ src/test/java/TestQuestion5.java
@@ -41,28 +41,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(MockitoExtension.class)
 public class TestQuestion5 {
 
-    private Stream<String> fullNameSrteam = Stream.of("John Lennon", "Paul McCartney", "George Harrison",
-            "Ringo Starr", "Pete Best", "Stuart Sutcliffe");
+    private Stream<String> getFullNameStream() {
+        return Stream.of("John Lennon", "Paul McCartney", "George Harrison",
+                "Ringo Starr", "Pete Best", "Stuart Sutcliffe");
+    }
 
-    private Stream<String> nameStream = Stream.of("John", "Paul", "George", "John", "Paul", "John");
+    private Stream<String> getNameStream() {
+        return Stream.of("John", "Paul", "George", "John", "Paul", "John");
+    }
 
     @Test
     public void testGetLongestNameReduce() {
-        String longestName = new Questions5().getLongestNameReduce(fullNameSrteam);
+        String longestName = new Questions5().getLongestNameReduce(getFullNameStream());
         System.out.println(String.format("Test result for %s: %s", "testGetLongestNameReduce", longestName));
         assertEquals("Stuart Sutcliffe", longestName);
     }
 
     @Test
     public void testGetLongestNameCollector() {
-        String longestName = new Questions5().getLongestNameCollector(fullNameSrteam);
+        String longestName = new Questions5().getLongestNameCollector(getFullNameStream());
         System.out.println(String.format("Test result for %s: %s", "testGetLongestNameCollector", longestName));
         assertEquals("Stuart Sutcliffe", longestName);
     }
 
     @Test
     public void testGetNamesCountMap() {
-        Map<String, Long> calculatedNames = new Questions5().getNameAmountMap(nameStream);
+        Map<String, Long> calculatedNames = new Questions5().getNameAmountMap(getNameStream());
         System.out.println(String.format("Calculated names: %s", calculatedNames));
         assertEquals(3, calculatedNames.size());
         assertEquals(Long.valueOf(3), calculatedNames.get("John"));
