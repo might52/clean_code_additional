be4fa9c069f8640bc8cbc758c57027d7b5009f6d
Andrei F._ <answers_com@hotmail.com>
2024-09-25 11:08:34 +0300

    added completionService test.

--- src/test/java/ConcurrentTest.java
+++ src/test/java/ConcurrentTest.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionService;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -57,6 +59,34 @@ public class ConcurrentTest {
         });
         exec.awaitTermination(5, TimeUnit.SECONDS);
         exec.shutdownNow();
+    }
+
+    @Test
+    public void completionServiceTestTasks() throws InterruptedException {
+        List<Callable<String>> tasks = new ArrayList<>();
+        for (int i = 0; i < NUMBER_OF_THREADS; i++) {
+            int finalInt = i;
+            tasks.add(() -> String.format("Callable number: {%d} with return value: {%d} %s:%d, datetime: %s",
+                finalInt, finalInt, Thread.currentThread().getName(),
+                Thread.currentThread().threadId(), System.currentTimeMillis()));
+        }
+        CompletionService<String> completionService = new ExecutorCompletionService<>(exec);
+        tasks.forEach(completionService::submit);
+        tasks.forEach(task -> {
+            try {
+                Future<String> future = completionService.take();
+                String value = future.get();
+                System.out.printf("Future value: %S state: %s, isDone: %s, isCancelled: %s%n", value, future.state(),
+                    future.isDone(), future.isCancelled());
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            } catch (ExecutionException e) {
+                System.out.println("Something went wrong during execution process: " + e.getCause());
+            }
+        });
+        exec.awaitTermination(5, TimeUnit.SECONDS);
+        exec.shutdownNow();
+
     }
 
 }
