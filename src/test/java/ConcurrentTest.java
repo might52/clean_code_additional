fe1e52b8d42cec1483399627e1a9c17f3e01e4bf
Andrei F._ <answers_com@hotmail.com>
2024-09-25 11:08:33 +0300

    added callable test.

--- src/test/java/ConcurrentTest.java
+++ src/test/java/ConcurrentTest.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +12,9 @@ public class ConcurrentTest {
 
     private static final int NUMBER_OF_THREADS = 10;
     private static final ExecutorService exec = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+    private static final ExecutorService cachedExec = Executors.newCachedThreadPool();
+    private static final ExecutorService singleExec = Executors.newSingleThreadExecutor();
+    private static final ExecutorService scheduledExec = Executors.newScheduledThreadPool(NUMBER_OF_THREADS);
 
     @Test
     public void testPerTask() throws InterruptedException {
@@ -23,6 +29,31 @@ public class ConcurrentTest {
             );
         }
         tasks.forEach(exec::submit);
+        exec.awaitTermination(5, TimeUnit.SECONDS);
+        exec.shutdownNow();
+    }
+
+    @Test
+    public void futureTestTasks() throws InterruptedException {
+        List<Callable<String>> tasks = new ArrayList<>();
+        for (int i = 0; i < NUMBER_OF_THREADS; i++) {
+            int finalInt = i;
+            tasks.add(() -> String.format("Callable number: {%d} with return value: {%d} %s:%d, datetime: %s",
+                finalInt, finalInt, Thread.currentThread().getName(),
+                Thread.currentThread().threadId(), System.currentTimeMillis()));
+        }
+        tasks.forEach(task -> {
+            Future<String> future = exec.submit(task);
+            try {
+                String value = future.get();
+                System.out.printf("Future value: %S state: %s, isDone: %s, isCancelled: %s%n", value, future.state(),
+                    future.isDone(), future.isCancelled());
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            } catch (ExecutionException e) {
+                System.out.println("Something went wrong during execution process: " + e.getCause());
+            }
+        });
         exec.awaitTermination(5, TimeUnit.SECONDS);
         exec.shutdownNow();
     }
