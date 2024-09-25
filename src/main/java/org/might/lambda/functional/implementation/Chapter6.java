f8888f65799d86b9d7f1bb85d0c6b27cf8fd3cf5
Andrei F._ <answers_com@hotmail.com>
2024-09-25 11:08:30 +0300

    chapter 6. fixed multiply Throwing

--- src/main/java/org/might/lambda/functional/implementation/Chapter6.java
+++ src/main/java/org/might/lambda/functional/implementation/Chapter6.java
@@ -1,5 +1,6 @@
 package org.might.lambda.functional.implementation;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class Chapter6 {
@@ -16,4 +17,16 @@ public class Chapter6 {
                 .map(x -> x * x)
                 .sum();
     }
+
+    public static int multiplyThrough(List<Integer> linkedListOfNumbers) {
+        return linkedListOfNumbers.stream()
+                .reduce(5, (acc, x) -> acc * x);
+    }
+
+    public static int multiplyThroughParallel(List<Integer> linkedListOfNumbers) {
+        return 5 * linkedListOfNumbers
+                .parallelStream()
+                .reduce(1, (acc, x) -> x * acc);
+    }
+
 }
--- src/test/java/Chapter6Test.java
+++ src/test/java/Chapter6Test.java
@@ -3,6 +3,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.might.lambda.functional.implementation.Chapter6;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,5 +22,14 @@ public class Chapter6Test {
     public void parallelSumOfSquaresTest() {
         assertEquals(5, Chapter6.parallelSumOfSquares(IntStream.range(1, 3)));
         assertEquals(328350, Chapter6.parallelSumOfSquares(IntStream.range(1, 100)));
+    }
+
+    @Test
+    public void multipleThrowTest() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3);
+        int resultSequential = Chapter6.multiplyThrough(numbers);
+        int resultParallel = Chapter6.multiplyThroughParallel(numbers);
+        assertEquals(30, resultSequential);
+        assertEquals(30, resultParallel);
     }
 }
