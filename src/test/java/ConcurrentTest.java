bfbc26b57880c10ac6234e7aa46c2449da8bf539
Andrei F._ <answers_com@hotmail.com>
2024-09-25 11:08:33 +0300

    corrected execution flow.

--- src/test/java/ConcurrentTest.java
+++ src/test/java/ConcurrentTest.java
@@ -1,15 +1,14 @@
-import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.Test;
 
 public class ConcurrentTest {
-    private static final int NUMBER_OF_THREADS = 4;
+
+    private static final int NUMBER_OF_THREADS = 10;
     private static final ExecutorService exec = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
-    private boolean isActive;
 
     @Test
     public void testPerTask() throws InterruptedException {
@@ -17,27 +16,15 @@ public class ConcurrentTest {
         for (int i = 0; i < NUMBER_OF_THREADS; i++) {
             int finalInt = i;
             tasks.add(() ->
-                    System.out.println("Number of the task: {" + finalInt + "}" +
+                System.out.println("Number of the task: {" + finalInt + "} " +
                             Thread.currentThread().getName() + ": " +
-                            Thread.currentThread().threadId())
+                    Thread.currentThread().threadId() +
+                    ", datetime: " + System.currentTimeMillis())
             );
         }
-        startServer(tasks);
-        stopServer();
-    }
-
-    @Test
-    public void testWithinTask() {
-
-    }
-
-    private void startServer(List<Runnable> tasks) {
         tasks.forEach(exec::submit);
-    }
-
-    private void stopServer() throws InterruptedException {
-        isActive = false;
-        exec.awaitTermination(30, TimeUnit.SECONDS);
+        exec.awaitTermination(5, TimeUnit.SECONDS);
         exec.shutdownNow();
     }
+
 }
