20f0647411baa6620a2c10d2a8838e20e1a2955d
Andrei F._ <answers_com@hotmail.com>
2024-09-25 11:08:30 +0300

    removed incorrect links

--- src/main/java/org/might/clientserver/common/MessageUtils.java
+++ src/main/java/org/might/clientserver/common/MessageUtils.java
@@ -1,32 +1,8 @@
-/*
- This software is the confidential information and copyrighted work of
- NetCracker Technology Corp. ("NetCracker") and/or its suppliers and
- is only distributed under the terms of a separate license agreement
- with NetCracker.
- Use of the software is governed by the terms of the license agreement.
- Any use of this software not in accordance with the license agreement
- is expressly prohibited by law, and may result in severe civil
- and criminal penalties. 
- 
- Copyright (c) 1995-2023 NetCracker Technology Corp.
- 
- All Rights Reserved.
- 
-*/
-/*
- * Copyright 1995-2023 by NetCracker Technology Corp.,
- * University Office Park III
- * 95 Sawyer Road
- * Waltham, MA 02453
- * United States of America
- * All rights reserved.
- */
 package org.might.clientserver.common;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.Socket;
@@ -49,23 +25,3 @@ public class MessageUtils {
          return objectInputStream.readUTF();
      }
 }
-/*
- WITHOUT LIMITING THE FOREGOING, COPYING, REPRODUCTION, REDISTRIBUTION,
- REVERSE ENGINEERING, DISASSEMBLY, DECOMPILATION OR MODIFICATION
- OF THE SOFTWARE IS EXPRESSLY PROHIBITED, UNLESS SUCH COPYING,
- REPRODUCTION, REDISTRIBUTION, REVERSE ENGINEERING, DISASSEMBLY,
- DECOMPILATION OR MODIFICATION IS EXPRESSLY PERMITTED BY THE LICENSE
- AGREEMENT WITH NETCRACKER. 
- 
- THIS SOFTWARE IS WARRANTED, IF AT ALL, ONLY AS EXPRESSLY PROVIDED IN
- THE TERMS OF THE LICENSE AGREEMENT, EXCEPT AS WARRANTED IN THE
- LICENSE AGREEMENT, NETCRACKER HEREBY DISCLAIMS ALL WARRANTIES AND
- CONDITIONS WITH REGARD TO THE SOFTWARE, WHETHER EXPRESS, IMPLIED
- OR STATUTORY, INCLUDING WITHOUT LIMITATION ALL WARRANTIES AND
- CONDITIONS OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE,
- TITLE AND NON-INFRINGEMENT.
- 
- Copyright (c) 1995-2023 NetCracker Technology Corp.
- 
- All Rights Reserved.
-*/
--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -1,27 +1,3 @@
-/*
- This software is the confidential information and copyrighted work of
- NetCracker Technology Corp. ("NetCracker") and/or its suppliers and
- is only distributed under the terms of a separate license agreement
- with NetCracker.
- Use of the software is governed by the terms of the license agreement.
- Any use of this software not in accordance with the license agreement
- is expressly prohibited by law, and may result in severe civil
- and criminal penalties. 
- 
- Copyright (c) 1995-2024 NetCracker Technology Corp.
- 
- All Rights Reserved.
- 
-*/
-/*
- * Copyright 1995-2024 by NetCracker Technology Corp.,
- * University Office Park III
- * 95 Sawyer Road
- * Waltham, MA 02453
- * United States of America
- * All rights reserved.
- */
-
 import org.junit.jupiter.api.Test;
 import org.might.lambda.functional.implementation.Fibonacci;
 
@@ -39,10 +15,9 @@ public class FibonacciTest {
     @Test
     public void fibonacciMatchesOpeningSequence() {
         List<Long> fibonacciSequence = Arrays.asList(0L, 1L, 1L, 2L, 3L, 5L, 8L, 13L, 21L, 34L);
-
+        Fibonacci fibonacci = new Fibonacci();
         IntStream.range(0, fibonacciSequence.size())
                 .forEach(x -> {
-                    Fibonacci fibonacci = new Fibonacci();
                     long result = fibonacci.fibonacci(x);
                     long expectedResult = fibonacciSequence.get(x);
                     assertEquals(expectedResult, result);
@@ -50,23 +25,3 @@ public class FibonacciTest {
     }
 
 }
-/*
- WITHOUT LIMITING THE FOREGOING, COPYING, REPRODUCTION, REDISTRIBUTION,
- REVERSE ENGINEERING, DISASSEMBLY, DECOMPILATION OR MODIFICATION
- OF THE SOFTWARE IS EXPRESSLY PROHIBITED, UNLESS SUCH COPYING,
- REPRODUCTION, REDISTRIBUTION, REVERSE ENGINEERING, DISASSEMBLY,
- DECOMPILATION OR MODIFICATION IS EXPRESSLY PERMITTED BY THE LICENSE
- AGREEMENT WITH NETCRACKER. 
- 
- THIS SOFTWARE IS WARRANTED, IF AT ALL, ONLY AS EXPRESSLY PROVIDED IN
- THE TERMS OF THE LICENSE AGREEMENT, EXCEPT AS WARRANTED IN THE
- LICENSE AGREEMENT, NETCRACKER HEREBY DISCLAIMS ALL WARRANTIES AND
- CONDITIONS WITH REGARD TO THE SOFTWARE, WHETHER EXPRESS, IMPLIED
- OR STATUTORY, INCLUDING WITHOUT LIMITATION ALL WARRANTIES AND
- CONDITIONS OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE,
- TITLE AND NON-INFRINGEMENT.
- 
- Copyright (c) 1995-2024 NetCracker Technology Corp.
- 
- All Rights Reserved.
-*/
