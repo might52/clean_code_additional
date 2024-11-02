a5c06d598814afd185208505703f59a482ec74fe
Andrei F._ <answers_com@hotmail.com>
2024-11-02 23:56:25 +0300

    added Deikstra alg example.

--- src/test/java/AlgorithmChapter6.java
+++ src/test/java/AlgorithmChapter6.java
@@ -60,4 +60,63 @@ public class AlgorithmChapter6 {
     private boolean personIsSeller(String person) {
         return person.endsWith("m");
     }
+
+    @Test
+    public void deikstraTest() {
+        Map<String, Map<String, Integer>> graph = new HashMap<>();
+        Map<String, Integer> level = new HashMap<>();
+        level.put("a", 6);
+        level.put("b", 2);
+        graph.put("start", level);
+        level = new HashMap<>();
+        level.put("fin", 1);
+        graph.put("a", level);
+        level = new HashMap<>();
+        level.put("a", 3);
+        level.put("fin", 5);
+        graph.put("b", level);
+        level = new HashMap<>();
+        graph.put("fin", level);
+        Map<String, Integer> costs = new HashMap<>();
+        costs.put("a", 6);
+        costs.put("b", 2);
+        costs.put("fin", 1000);
+        Map<String, String> parents = new HashMap<>();
+        parents.put("a", "start");
+        parents.put("b", "start");
+        parents.put("fin", "None");
+        System.out.println("Lowest cost to find proper way: " + findLowestCost(graph, costs, parents));
+    }
+
+    private String findLowestCost(Map<String, Map<String, Integer>> graph, Map<String, Integer> costs, Map<String, String> parents) {
+        List<String> processed = new ArrayList<>();
+        String node = findLowestCostNode(costs, processed);
+        while (node != null) {
+            Integer cost = costs.get(node);
+            Map<String, Integer> neighbors = graph.get(node);
+            for (String neighbor : neighbors.keySet()) {
+                Integer newCost = cost + neighbors.get(neighbor);
+                if (costs.get(neighbor) > newCost) {
+                    costs.put(neighbor, newCost);
+                    parents.put(neighbor, node);
+                }
+            }
+            processed.add(node);
+            node = findLowestCostNode(costs, processed);
+        }
+        return parents.get("fin");
+    }
+
+    private String findLowestCostNode(Map<String, Integer> costs, List<String> processed) {
+        Integer lowestCost = 1000;
+        String lowestCostNode = null;
+        for (String node : costs.keySet()) {
+            Integer cost = costs.get(node);
+            if (cost < lowestCost && !processed.contains(node)) {
+                lowestCost = cost;
+                lowestCostNode = node;
+            }
+        }
+        return lowestCostNode;
+    }
 }
